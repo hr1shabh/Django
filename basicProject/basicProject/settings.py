421addb710dcfb6abd7cd7581f01ffe176a8654a
Hrishabh <hrishabhpalsra@gmail.com>
2024-12-03 21:57:08 +0530

    added model Cars and migrated it

--- basicProject/basicProject/settings.py
+++ basicProject/basicProject/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'demo',
+    'tailwind',
+    'theme',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- basicProject/demo/models.py
+++ basicProject/demo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Cars(models.Model):
+    car_type = [
+        ('S', 'Sedan'),
+        ('Su', 'SUV'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='demos/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=car_type)
+
+    def __str__(self):
+        return self.name
