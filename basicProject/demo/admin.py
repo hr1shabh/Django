69c922520b4a1a724df6734232367af1716d0712
Hrishabh <hrishabhpalsra@gmail.com>
2024-12-04 23:42:57 +0530

    make relations one to one, many to many, one to many and customize admin

--- basicProject/demo/admin.py
+++ basicProject/demo/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import Cars
+from .models import Cars, CarReviews, Store, CarCertificate
 # Register your models here.
 
-admin.site.register(Cars)
+class CarReviewsInline(admin.TabularInline):
+    model = CarReviews
+    extra = 2
+
+class CarsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [CarReviewsInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('car',)
+
+class CarCertificateAdmin(admin.ModelAdmin):
+    list_display = ('car', 'certificate_number')
+
+admin.site.register(Cars, CarsAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(CarReviews)
+admin.site.register(CarCertificate, CarCertificateAdmin)
+
+
--- basicProject/demo/models.py
+++ basicProject/demo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 class Cars(models.Model):
     car_type = [
@@ -14,3 +15,35 @@ class Cars(models.Model):
 
     def __str__(self):
         return self.name
+
+
+## One to many car - many reviews
+## Foreign key means one to many
+class CarReviews(models.Model):
+    car = models.ForeignKey(Cars, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.car.name}'
+
+## many to many
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    car = models.ManyToManyField(Cars, related_name='stores')
+
+    def __str__(self):
+        return self.name   
+    
+## one to one
+
+class CarCertificate(models.Model):
+    car = models.OneToOneField(Cars, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return f'certificate for {self.car}'
